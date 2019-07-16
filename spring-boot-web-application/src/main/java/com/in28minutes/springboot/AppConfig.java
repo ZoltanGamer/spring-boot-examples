e44c66320b17f80d1b3d7c071f06c8ac751a134e
Kulcsár Zoltán <zoltan_kulcsar@epam.com>
2019-07-16 14:55:37 +0200

    transformed jsp to ftl

---
+++ spring-boot-web-application/src/main/java/com/in28minutes/springboot/AppConfig.java
@@ -0,0 +1,28 @@
+package com.in28minutes.springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver(){
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer(){
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/ftl/");
+        return configurer;
+    }
+
+
+}
---
+++ spring-boot-web-application/src/main/webapp/WEB-INF/ftl/welcome.ftl
@@ -0,0 +1,3 @@
+<div class="container">
+    Welcome ${.globals.name!"John Doe, mert nincs meg a nev"}!!
+</div>
